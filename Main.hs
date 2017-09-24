acf4e806396fbe93771150e2f578fc1909cefb37
Mike J Hewitt <mikeh@electroteach.com>
2017-09-24 22:55:16 +1000

    after hindent

--- Main.hs
+++ Main.hs
@@ -1,38 +1,46 @@
 {-# OPTIONS_GHC -fwarn-missing-signatures #-}
+
+import Control.Applicative
+import Control.Monad
+import Data.Time
 import System.Environment
 import System.IO
-import Control.Monad
 import System.Process
-import Control.Applicative
 import Text.Printf
-import Data.Time
 
 sequenceWhileTrue :: Monad m => [m Bool] -> m ()
 sequenceWhileTrue [] = return ()
-sequenceWhileTrue (m:ms) = do 
-                r <- m 
-                if r then m >> sequenceWhileTrue ms
-                     else return ()
+sequenceWhileTrue (m:ms) = do
+  r <- m
+  if r
+    then m >> sequenceWhileTrue ms
+    else return ()
+
 main :: IO ()
 main = do
-       (x:xs) <- getArgs
-       (_, Just hout, _, _) <-
-          createProcess (proc x xs) {std_out = CreatePipe}
-       sequenceWhileTrue $
-         ((flip map) [1..] (\x -> do
-          ((more,line0),time0) <- timeAction $ do 
-                                              more <-  not <$> hIsEOF hout  
-                                              if more then do 
-                                                           hout' <- hGetLine hout
-                                                           return (more,hout')
-                                                      else return (more,"")
-          when more (putStrLn $ show x ++ ":" ++  printf "%f" (time0) ++ ":" ++ line0)
+  (x:xs) <- getArgs
+  (_, Just hout, _, _) <- createProcess (proc x xs) {std_out = CreatePipe}
+  sequenceWhileTrue $
+    ((flip map)
+       [1 ..]
+       (\x -> do
+          ((more, line0), time0) <-
+            timeAction $ do
+              more <- not <$> hIsEOF hout
+              if more
+                then do
+                  hout' <- hGetLine hout
+                  return (more, hout')
+                else return (more, "")
+          when
+            more
+            (putStrLn $ show x ++ ":" ++ printf "%f" (time0) ++ ":" ++ line0)
           return more))
 
 timeAction :: IO a -> IO (a, Float)
 timeAction action = do
-    t1 <- getCurrentTime
-    g <- action
-    t2 <- getCurrentTime
-    let timeInUnits = (realToFrac $ diffUTCTime t2 t1 :: Float)
-    return (g,timeInUnits)
+  t1 <- getCurrentTime
+  g <- action
+  t2 <- getCurrentTime
+  let timeInUnits = realToFrac $ diffUTCTime t2 t1 :: Float
+  return (g, timeInUnits)
